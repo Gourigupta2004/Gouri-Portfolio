a5c601782837417e3d91251c0fb521568e4a308c
Gourigupta2004 <guptagouri89@gmail.com>
2025-06-29 21:23:40 +0530

    fixed about page UI

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,12 +5,12 @@ import { Fade } from 'react-awesome-reveal';
 function About() {
   return (
     <section id="about" className="about-section">
-      <div className="container row">
+      <div className="container row ml-0">
         <div className="col-md-4 mt-4 mb-4 mb-md-0" style={{ display: 'flex', justifyContent: 'center', alignItems: 'start' }}>
           <h2 className="about-heading neon-text">ABOUT ME</h2>
         </div>
         <div className="col-md-8 text-center mt-3 mb-4 mb-md-0" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <Fade direction="right" triggerOnce={false}>
+        <Fade direction="right" triggerOnce={false} easing="ease-in-out">
             <div className="about-card p-5">
               <h3 className="about-name">GOURI<br />GUPTA</h3>
               <p className="about-description mt-4">
